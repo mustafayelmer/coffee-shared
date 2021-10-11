bdc31bba7dc367f9fd088fe27f0611b2e3f06968
Mustafa Yelmer <m.yelmer@azerion.com>
2021-10-11 17:19:11 +0200

    Weather.calculatedAt : Date>string

--- src/weather/Weather.ts
+++ src/weather/Weather.ts
@@ -3,6 +3,6 @@ import {WeatherRoot} from "./WeatherRoot";
 export interface Weather {
     id: string;
     city: string;
-    calculatedAt: Date;
+    calculatedAt: string;
     doc: WeatherRoot;
 }
